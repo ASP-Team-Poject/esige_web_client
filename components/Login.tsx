06b001a3607063ef845aaca2cb3c6d0081d9fce8
pribakil <bakengailunga@gmail.com>
2025-05-05 03:51:32 +0100

    update: login page UI matched to Mobile App one

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,34 +2,26 @@
 
 import { globalImages } from "@/util/images";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import ESigeLogo from "./basic/ESigeLogo";
-import { User, Lock, LogIn } from "lucide-react";
+import { User, Lock, LogIn, Facebook, LockOpen } from "lucide-react";
 import H1 from "./basic/H1";
 import Input from "./basic/Input";
 import { useRouter } from "next/navigation";
 import Button from "./basic/Button";
 
 const Login = () => {
+  const [passwordType, setPasswordType] = useState<string>("password");
   const router = useRouter();
   const handleSubmit = (e: any) => {
     e.preventDefault();
     router.push("/home");
   };
   return (
-    <div className="flex flex-wrap justify-center items-center gap-16">
-      <div>
-        <Image
-          src={globalImages.asp_logo}
-          alt="asp logo"
-          width={500}
-          height={500}
-        />
-      </div>
-
+    <div className="flex flex-col justify-center items-center gap-8">
       <form
         onSubmit={(e) => handleSubmit(e)}
-        className="flex flex-col items-center gap-8 px-4 py-8 bg-white rounded-lg shadow-2xl max-w-[25rem]"
+        className="flex flex-col items-center gap-6 px-4 py-6 bg-white rounded-lg shadow-2xl max-w-[25rem]"
       >
         <div className="flex flex-col items-center gap-4">
           <ESigeLogo />
@@ -37,6 +29,9 @@ const Login = () => {
             title={"Connectez-vous à votre compte"}
             className="mx-4 text-center"
           />
+          <label className="text-gray-500 text-sm">
+            {"Vous n'avez pas de compte ? contactez l'administrateur"}
+          </label>
         </div>
 
         <div className="flex flex-col gap-4 w-full">
@@ -45,11 +40,29 @@ const Login = () => {
             placeholder="Nom d'utilisateur"
             icon={<User color="gray" />}
           />
+
           <Input
-            type="password"
+            type={passwordType}
             placeholder="**********"
-            icon={<Lock color="gray" />}
+            icon={
+              passwordType === "password" ? (
+                <Lock
+                  color="gray"
+                  onClick={() => setPasswordType("text")}
+                  className="cursor-pointer"
+                />
+              ) : (
+                <LockOpen
+                  color="gray"
+                  onClick={() => setPasswordType("password")}
+                  className="cursor-pointer"
+                />
+              )
+            }
           />
+          <label className="text-gray-500 text-sm">
+            {"Mot de passe oublié ? Veuillez contacter l'administrateur"}
+          </label>
         </div>
 
         <Button
@@ -58,7 +71,38 @@ const Login = () => {
           title="Connectez - vous"
           icon={<LogIn />}
         />
+
+        <div className="w-full flex flex-col gap-4 justify-center items-center">
+          <div className="w-[80%] relative border-b-[1px] border-[#ccc] flex justify-center">
+            <label className="w-fit absolute -top-3 bg-white text-gray-500 text-sm">
+              {"Ou connectez-vous avec"}
+            </label>
+          </div>
+          <div className="flex gap-4 justify-center items-center">
+            <Image
+              src={globalImages.facebook}
+              height={25}
+              width={25}
+              alt="facebook"
+            />
+            <Image
+              src={globalImages.google}
+              height={25}
+              width={25}
+              alt="google"
+            />
+          </div>
+        </div>
       </form>
+
+      <div>
+        <Image
+          src={globalImages.asp_logo}
+          alt="asp logo"
+          width={50}
+          height={50}
+        />
+      </div>
     </div>
   );
 };
--- util/images.ts
+++ util/images.ts
@@ -1,7 +1,9 @@
-const IMAGES_BASE_PATH = "/images/"
+const IMAGES_BASE_PATH = "/images/";
 
 export const globalImages = {
-    esige_logo : `${IMAGES_BASE_PATH}esigelogo.png`,
-    asp_logo : `${IMAGES_BASE_PATH}asplogo.png`,
-    coming_soon : `${IMAGES_BASE_PATH}under_construction.svg`
-}
+  esige_logo: `${IMAGES_BASE_PATH}esigelogo.png`,
+  asp_logo: `${IMAGES_BASE_PATH}asplogo.png`,
+  coming_soon: `${IMAGES_BASE_PATH}under_construction.svg`,
+  google: `${IMAGES_BASE_PATH}google.png`,
+  facebook: `${IMAGES_BASE_PATH}facebook.png`,
+};
